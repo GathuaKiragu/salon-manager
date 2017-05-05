63b3aa94a8ac16f4666033ac6cc75b417bdeffa0
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-05 13:09:06 +0300

    All instanciates correctly

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,13 +7,39 @@ import java.util.List;
    private String availability;
    private int id;
    private String image;
+   private String style;
    private int styListId;
 
-    public Client(String name, String phone, String availability, String image, int styListId){
+    public Client(String name, String phone, String availability, String image, String style, int styListId){
         this.name = name;
         this.phone = phone;
         this.availability = availability;
         this.image = image;
+        this.style = style;
         this.styListId = styListId;
+     }
+
+     public String getName() {
+       return name;
+     }
+// This Returns the client phone number
+     public String getPhone() {
+       return phone;
+     }
+// This returns the availability of the client
+     public String getAvailability() {
+       return availability;
+     }
+// This returns the clients image
+     public String getImage() {
+       return image;
+     }
+// This returns the category id
+     public int getStyListId() {
+       return styListId;
+     }
+
+     public String getStyle() {
+       return style;
      }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -6,7 +6,7 @@ public class ClientTest {
   private Client myClient;
   @Before
   public void instanciate(){
-  myClient = new Client("Vanessa Musera", "254777885596" , "Morning", "Crothets", 1);
+  myClient = new Client("Vanessa Musera", "254777885596" , "Morning", "pictureurl", "Crothets", 1);
   }
 
   // Test to check instance of client
@@ -14,4 +14,37 @@ public class ClientTest {
     public void client_instantiatesCorrectly_true() {
       assertEquals(true, myClient instanceof Client);
     }
+
+    // Test to check if client instanciates correctly with client name
+      @Test
+
+      public void ClientInstantiatesCorrectlywithName_true() {
+        assertEquals("Vanessa Musera", myClient.getName());
+      }
+      // Test to check if client instanciates correctly with client phone number
+      @Test
+
+      public void ClientInstantiatesCorrectlywithPhone_true() {
+        assertEquals("254777885596", myClient.getPhone());
+      }
+      // Test to check if client instanciates correctly with availability
+
+      @Test
+
+      public void ClientInstantiatesCorrectlywithAbout_true() {
+        assertEquals("Morning", myClient.getAvailability());
+      }
+
+    // Test to check if client instanciates correctly with StylistId
+      @Test
+
+      public void ClientInstantiatesCorrectlywithPicture_true() {
+        assertEquals("pictureurl", myClient.getImage());
+      }
+
+      @Test
+      public void ClientInstantiatesCorrectlywithStyListId_true() {
+        assertEquals(1, myClient.getStyListId());
+      }
+
 }
