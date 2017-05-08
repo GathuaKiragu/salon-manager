7043e2369fb603f916a071bdb51696fd4af2bb40
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-08 12:40:38 +0300

    Updated client and stylist to display images

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -109,6 +109,7 @@ public class App {
     get("/stylists/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      String image = request.queryParams("image");
       model.put("stylist", stylist);
       model.put("template", "templates/stylist.vtl");
       return new ModelAndView(model, layout);
