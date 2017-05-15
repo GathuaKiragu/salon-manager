7c76f6baf02e71e260a81621e73a51f8759bb7c5
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-15 18:41:54 +0300

    Updated db.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,9 +1,28 @@
 import org.sql2o.*;
-
-// public class DB {
-//   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "kiragu", "brianmbete");
-// }
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-107-21-248-129.compute-1.amazonaws.com:5432/degirc20hojegr","mhikpqslmnkbzr", "39f8425816fba377f1fdfa14758dd838a7189e39f00f5a7b5515596ee1971ae2");
+    private static URI dbUri;
+    public static Sql2o sql2o;
+
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/hair_salon");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "kiragu" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "brianmbete" : dbUri.getUserInfo().split(":")[1];
+
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+        }
+    }
 }
